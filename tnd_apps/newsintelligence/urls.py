feb3bd71e7c8100816f28521cc5a54a9b68e4b61
pks <pklab2>
2026-02-16 01:06:29 +0300

    update on diject

--- tnd_apps/newsintelligence/urls.py
+++ tnd_apps/newsintelligence/urls.py
@@ -1,12 +1,12 @@
 # urls.py
 from django.urls import path
-from . import views
+from .views import  DailyDigestListView, DailyDigestDetailView
 
 app_name = 'digests'  # optional
 
 urlpatterns = [
-    path('digests/', views.DailyDigestListView.as_view(), name='digest-list'),
-    path('digests/<int:pk>/', views.DailyDigestDetailView.as_view(), name='digest-detail'),
+    path('digests/', DailyDigestListView.as_view(), name='digest-list'),
+    path('digests/<int:pk>/', DailyDigestDetailView.as_view(), name='digest-detail'),
 
     # Alternative: date-based lookup (cleaner for sharing)
     # path('digests/<date:digest_date>/', views.DailyDigestDetailView.as_view(lookup_field='digest_date'), name='digest-by-date'),
